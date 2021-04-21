86314c1eadc55c834e783c395193767aa6bedd81
Valery <chinvalery@gmail.com>
2021-04-21 16:36:37 +0200

    [add bookrouter] add bookRouter

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,17 @@
 import userRouter from './User';
 import authorRouter from './Author';
+import bookRouter from './Book';
+
+//Associations 
+class Associations {
+
+  static associate(book) {
+    // define association here
+
+    return this;
+  }
+}
 
 
-const routes = [userRouter, authorRouter];
+const routes = [userRouter, authorRouter, bookRouter];
 export default routes;
