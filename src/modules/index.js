4c1df7dfa2faf312023ba638576a3a6f14aa8b0f
Valery <chinvalery@gmail.com>
2021-04-21 18:35:52 +0200

    [test association class with Dao models]

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,4 @@
+// Routes
 import userRouter from './User';
 import authorRouter from './Author';
 import bookRouter from './Book';
@@ -7,26 +8,37 @@ import mangaRouter from './TypeOfBook/Manga';
 import novelRouter from './TypeOfBook/Novel';
 import pocketBookRouter from './TypeOfBook/PocketBook';
 
-//Associations 
+const routes =
+  [
+    userRouter,
+    comicsRouter,
+    journalRouter,
+    mangaRouter,
+    novelRouter,
+    pocketBookRouter,
+    authorRouter,
+    bookRouter
+  ];
+
+
+// Associations 
+import { UserDao } from './User';
+import { AuthorDao } from './Author';
+import { BookDao } from './Book';
+import { ComicsDao } from './TypeOfBook/Comics';
+import { JournalDao } from './TypeOfBook/Journal';
+import { MangaDao } from './TypeOfBook/Manga';
+import { NovelDao } from './TypeOfBook/Novel';
+import { PocketBookDao } from './TypeOfBook/PocketBook';
+
 class Associations {
-
-  static associate(book) {
-    // define association here
-
+  static associate() {
+    AuthorDao.belongsToMany(BookDao, { through: "copyright" });
+    BookDao.belongsToMany(AuthorDao, { through: "copyright" });
     return this;
   }
 }
 
-const routes = 
-  [
-    userRouter, 
-    comicsRouter, 
-    journalRouter, 
-    mangaRouter,
-    novelRouter, 
-    pocketBookRouter, 
-    authorRouter,
-    bookRouter
-  ];
+Associations.associate()
 
 export default routes;
