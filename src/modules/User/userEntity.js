0f3a628e15dd7e765bb14ef5d3bdf0a469ebc465
ValeryC <36843384+ValeryC@users.noreply.github.com>
2021-04-22 20:58:25 +0200

    Update userEntity.js
    add l12!this.lastName

--- src/modules/User/userEntity.js
+++ src/modules/User/userEntity.js
@@ -2,14 +2,14 @@ class UserEntity {
 
   constructor({ firstName, lastName, email, password, role }) {
     this.firstName = firstName,
-      this.lastName = lastName,
-      this.email = email;
+    this.lastName = lastName,
+    this.email = email;
     this.password = password;
     this.role = null;
   }
 
   validate() {
-    if (!this.firstName || this.lastName || !this.email || !this.password || !this.role)
+    if (!this.firstName || !this.lastName || !this.email || !this.password || !this.role)
       return false;
     else
       return true;
