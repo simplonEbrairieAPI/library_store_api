b2e0c933422e00cdade1314e43951479ff7f359f
Valery <chinvalery@gmail.com>
2021-04-22 20:42:04 +0200

    [drop userDao key change to UserDao]

--- src/modules/User/index.js
+++ src/modules/User/index.js
@@ -1,19 +1,17 @@
 import { Router } from 'express';
 import { DataTypes } from 'sequelize';
-import db from '../../config/database';
+// import db from '../../config/database';
 import bcrypt from "bcrypt"
 
 import UserDao from './userDao';
-
 import UserRepository from './userRepository';
 import UserService from './userService';
 import UserController from "./userController"
 import UserRouter from './userRouter';
 
-
 const router = Router();
 
-const userRepository = new UserRepository({ userDao: UserDao, bcrypt });
+const userRepository = new UserRepository({ UserDao, bcrypt });
 const userService = new UserService({ userRepository });
 const userController = new UserController({ userService })
 const userRouter = new UserRouter({ router, userController });
