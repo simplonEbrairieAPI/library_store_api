aae94df18cc2867010cbcaa022100a88e477e48b
Valery <chinvalery@gmail.com>
2021-04-22 20:36:23 +0200

    [add bookinit]add as, belongsTo

--- src/modules/Book/bookDao.js
+++ src/modules/Book/bookDao.js
@@ -1,4 +1,5 @@
-import { Model } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+import db from '../../config/database';
 
 class Book extends Model {
   static init(sequelize, DataTypes) {
@@ -7,23 +8,24 @@ class Book extends Model {
         title: DataTypes.STRING,
         description: DataTypes.STRING,
         editors: DataTypes.STRING,
-        authorId: DataTypes.INTEGER,
-      }, { sequelize, modelName: 'Book' }
+        // authorId: DataTypes.INTEGER,
+      }, { sequelize, modelName: 'Book', freezeTableName: true, timestamps: false }
     );
   }
 
   static associate(models) {
-
-    this.belongsTo(models.Author, 
+    this.belongsTo(models.Author,
       {
         foreignKey: {
           name: "authorId",
+          as: "Author"
         }
       });
 
     return this;
-
   }
 };
+
+Book.init(db.sequelize, DataTypes);
 
 export default Book;
