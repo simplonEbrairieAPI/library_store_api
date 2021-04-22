fd6f392438c36ef2145ff848346e507380ffa2cf
Valery <chinvalery@gmail.com>
2021-04-22 18:55:32 +0200

    [add parameters]add firstname lastname role

--- src/modules/User/userEntity.js
+++ src/modules/User/userEntity.js
@@ -1,13 +1,15 @@
 class UserEntity {
 
-  constructor({ id, email, password }) {
-    this.id = id;
-    this.email = email;
+  constructor({ firstName, lastName, email, password, role }) {
+    this.firstName = firstName,
+      this.lastName = lastName,
+      this.email = email;
     this.password = password;
+    this.role = null;
   }
 
   validate() {
-    if (!this.email || !this.password)
+    if (!this.firstName || this.lastName || !this.email || !this.password || !this.role)
       return false;
     else
       return true;
