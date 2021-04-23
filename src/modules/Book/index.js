0e251f1fbebfa188f4e6f655e4edc0e74ba3417a
Valery <chinvalery@gmail.com>
2021-04-23 17:49:08 +0200

    [add {}]

--- src/modules/Book/index.js
+++ src/modules/Book/index.js
@@ -1,19 +1,15 @@
 import { Router } from 'express';
-import { DataTypes } from 'sequelize';
-import db from '../../config/database';
 
 import BookDao from './bookDao';
 import BookRepository from './bookRepository';
 import BookService from './bookService';
-import BookController from "./bookController"
+import BookController from './bookController';
 import BookRouter from './bookRouter';
 
 const router = Router();
 
-// const bookDao = BookDao.init(db.sequelize, DataTypes);
-
 const bookRepository = new BookRepository(BookDao);
-const bookService = new BookService(bookRepository);
+const bookService = new BookService({ bookRepository });
 const bookController = new BookController({ bookService })
 const bookRouter = new BookRouter({ router, bookController });
 
