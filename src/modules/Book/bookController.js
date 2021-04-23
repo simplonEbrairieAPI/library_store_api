a92a52fcd66f2af60348a95eaf11ee05e7d929a6
Valery <chinvalery@gmail.com>
2021-04-23 17:21:22 +0200

    [add error.stack]can see type of error with console.error

--- src/modules/Book/bookController.js
+++ src/modules/Book/bookController.js
@@ -1,17 +1,16 @@
 class BookController {
-
   constructor({ bookService }) {
     this.bookService = bookService;
   }
 
   getAll = async ({ res }) => {
     try {
-      let users = await this.bookService.getAll();
+      let books = await this.bookService.getAll();
       console.log("want to know if we are in the try of method getAll", books)
       res.status(200).json(books);
     } catch (err) {
-      console.log("error in bookController.js .catch is functionnal ")
-      res.status(400).json(err.message + "  " + "big trouble in Bookcontroller.js");
+      console.error("error in bookController.js .catch is functionnal ", err.stack)
+      res.status(400).json("big trouble to get all books");
     }
   }
 }
