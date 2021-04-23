4d6996d29a95e6802324ee3af7fefbed1566b627
Valery <chinvalery@gmail.com>
2021-04-23 18:17:04 +0200

    [delete {}, clean register]

--- src/modules/User/userService.js
+++ src/modules/User/userService.js
@@ -1,6 +1,7 @@
 import UserEntity from './userEntity';
+
 class UserService {
-  constructor({ userRepository }) {
+  constructor(userRepository) {
     this.userRepo = userRepository;
   }
 
@@ -14,44 +15,15 @@ class UserService {
     return user
   }
 
-  // async register(userData) {
-  //   const userEntity = new UserEntity(userData);
-  //   console.log("qjqjqjqqjqjqjqjqj register of USERSERVICE ", userEntity)
-  //   // if (!userEntity.validate())
-  //   //   throw new Error('User entity validation error: Missing parameters');
+  async register(userData) {
+    const userEntity = new UserEntity({});
 
-  //   const newUser = await this.userRepo.createUser(userEntity);
-  //   console.log("newuser ca donne quoi service", newUser)
-
-  //   return new UserEntity(newUser);
-  // }
+    // if (!userEntity.validateProfile() || !userEntity.validateLogin()) throw new Error('User entity validation error: Missing parameters')
 
 
-  // async register(userData) {
-  //   const userEntity = new UserEntity(userData);
-  //   if (!userEntity.validate())
-  //     throw new this.ErrorHandler(400, 'Missing required email and password fields');
-
-  //   const newUser = await this.userRepo.create(userEntity);
-  //   await this.mailer.sendMail(userEntity);
-  //   return new UserEntity(newUser);
-  // }
-
-  // async login(userData) {
-  //   const userEntity = new UserEntity(userData);
-  //   if (!userEntity.validate())
-  //     throw new Error('User entity validation error: Missing parameters');
-
-  //   const user = await this.userRepo.findByEmail(userEntity);
-  //   if (!user)
-  //     throw new Error('Account do not exist');
-
-  //   const passwordMatch = await this.userRepo.compareHash(userEntity.password, user.password);
-  //   if (!passwordMatch)
-  //     throw new Error('Password do not match');
-
-  //   return new UserEntity(user);
+    const newUser = await this.userRepo.createUser(userData);
+    return new UserEntity(newUser);
+  }
 }
 
 export default UserService;
-
