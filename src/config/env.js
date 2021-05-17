6cd2259aa662567527e5d5497646f117b2a1c9ff
Valery <chinvalery@gmail.com>
2021-05-17 18:49:16 +0200

    [middleware]instance middlewares in server.js

--- src/config/env.js
+++ src/config/env.js
@@ -7,6 +7,7 @@ const config = {
   database_password: process.env.DB_PASSWORD || 'root',
   database_user: process.env.DB_USER || 'root',
   database_port: process.env.DB_PORT || 8889,
+  jwt_secret_key: process.env.JWT_SECRET
 }
 
 export default config;
