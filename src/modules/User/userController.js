f024354cba323b4012b23c8db2cc738d8cd25508
Valery <chinvalery@gmail.com>
2021-05-19 20:02:08 +0200

    correct cors authorization

--- src/modules/User/userController.js
+++ src/modules/User/userController.js
@@ -45,27 +45,11 @@ class UserController {
   login = async (req, res) => {
     try {
       const user = await this.userService.login({ ...req.body });
-      if (!user) {
-        res.status(200).json({
-          success: false,
-          error: "Email ou mot de passe incorrect"
-        })
-      } else {
-        const token = await this.jwtService.generateToken({ id: user.id });
-        res.status(200).cookie('token', token, {
-          // expires: new Date(Date.now() + 300000), // milisecondes = 5 min
-          secure: false, // set to true if your using https
-          httpOnly: true,
-        }).json({
-          success: true,
-          user: {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email
-          }
-          // cookie: `Expires at ${new Date(Date.now() + 300000)}`
-        });
-      }
+      const token = await this.jwtService.generateToken({ id: user.id });
+
+      res.cookie('auth-cookie', token, { expires: false, httpOnly: true });
+      // res.status(200).json(user.id);
+      res.status(200).json(token);
     } catch (err) {
       console.error(err);
       res.status(400).send({ error: err.message });
